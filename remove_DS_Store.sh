78336fe3ce3ef0990d93049b545231cabb5acdfc
natpacket <1399092738@qq.com>
2023-07-14 12:50:36 +0800

    提交更新

---
+++ remove_DS_Store.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+find . -name .DS_Store -print0 | xargs -0 git rm -f --ignore-unmatch
+
+echo .DS_Store >> .gitignore
+
+git add .gitignore
+git commit -m '.DS_Store banished!'
+git push
